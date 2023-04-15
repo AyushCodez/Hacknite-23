305087f94f70989649f835e0ba70ab8a25fadbd9
Ayush Gupta <37546890+AyushCodez@users.noreply.github.com>
2023-04-15 15:22:16 +0530

    I CAN HAZ COMMENTZ.

--- get_comments.py
+++ get_comments.py
@@ -2,28 +2,75 @@
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
-driver = webdriver.Chrome()
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
-def main(URL):
-    HEADERS = ({'User-Agent':'Chrome/44.0.2403.157','Accept-Language': 'en-US, en;q=0.5'})
-    webpage = requests.get(URL, headers=HEADERS)
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+options = webdriver.ChromeOptions() 
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+def get_keywords(URL):
+
     driver.get(URL)
+    time.sleep(1)
+    element = driver.find_element(By.CLASS_NAME,"cr-widget-TitleRatingsHistogram")
+
+    actions = ActionChains(driver)
+    actions.move_to_element(element).perform()
+    a = driver.find_elements(By.CLASS_NAME,"a-declarative")
+    
+    # ans = 0
+    
+    # for i in a:
+    #     if i.get_attribute("data-action") == "reviews:filter-action:apply":
+    #         ans+=1
+    #         print(i.text)
+    
+    # print(ans, "LOOK HERE")
+
     r = driver.page_source
 
 
-    soup = BeautifulSoup(r, "lxml")
+    soup = BeautifulSoup(r, "html.parser")
 
     with open("temp.html","w",encoding="utf-8") as f:
         f.write(str(soup))
 	
-    para = soup.find_all('a', attrs = {'data-reftag':"cm_cr_othr_d_lh_1"}) 
+    para = soup.find_all('span', attrs = {"data-action":"reviews:filter-action:apply"})
+    
+    for i in para:
+        print(i.text.strip())
+def get_links(keyword):
 
-    print(para)
+    HEADERS = ({'User-Agent':'Chrome/44.0.2403.157', 'Accept-Language': 'en-US, en;q=0.5'})
+    r = requests.get(f"https://www.amazon.in/s?k={keyword}", headers= HEADERS)
+
+    soup = BeautifulSoup(r.content,"html.parser")
+
+    with open("temp.html","w",encoding="utf-8") as f:
+        f.write(str(soup))
+	
+
+    elem = soup.find_all('div', attrs= {"data-component-type":"s-search-result"})
+
+    links = []
+    for i in elem:
+        temp = i.find('a')
+        t = temp["href"]
+        while "bestsellers" in t:
+            temp = temp.find_next('a')
+            t = temp["href"]
+        links.append("https://www.amazon.in" + t)
+    return links
 
 
 
 
 if __name__ == '__main__':
-    links = ["https://www.amazon.in/Xbox-Wireless-Controller-Carbon-Black/dp/B08K3FVP5N/"]
+    k = input()
+    links = get_links(k)
+    #links = ["https://www.amazon.in/Xbox-Series-S/dp/B08J89D6BW"]
     for link in links:
-        main(link)
+        print(link)
+        get_keywords(link)
