a4bdc18fa988083c1ddbb46307966e26ea0ce58e
Ayush Gupta <37546890+AyushCodez@users.noreply.github.com>
2023-04-16 13:49:28 +0530

    ebay works now.

--- key_sentiment.py
+++ key_sentiment.py
@@ -54,22 +54,50 @@ def get_sent(url: str, keys: list)->float:
 
         div1 = driver.find_element(By.CLASS_NAME, "reviews-right")
         div = div1.find_element(By.CLASS_NAME, "reviews-header")
-        actions = ActionChains(driver)
-        actions.move_to_element(div).perform()
-        print(div.text)
+
         
         time.sleep(1)
 
         button = div.find_element(By.CLASS_NAME, "sar-btn")
         button.click()
-        
-        r = driver.page_source
+        new_url = driver.current_url
         driver.close()
 
-        soup = BeautifulSoup(r, "html.parser")
-
-link = "https://www.amazon.in/HP-Gaming-Mouse-G200-7QV30AA/dp/B08498186T"
-link = "https://www.ebay.com/itm/185829460367"
-#keys = get_keywords(link)
-keys = ["battery"]
-get_sent(link,keys)
+        for i in keys:
+            temp = new_url + "&q=" + i.replace(" ","+")
+            response = requests.get(temp, headers=HEADERS)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            comment_elements = soup.find_all('div', {'class':'ebay-review-section'})
+            key_score = 0
+            comment_num = 0
+            for comment_element in comment_elements:
+                comment_score = 0
+                sent_num = 0
+                comment_text = comment_element.find('p', {'itemprop':'reviewBody'}).text
+                for sent in comment_text.split('.'):
+                    sent = sent.strip()
+                    if i in sent:
+                        sent_blob = TextBlob(sent)
+                        comment_score+=sent_blob.sentiment.polarity
+                        sent_num+=1
+                
+                if comment_score!=0:
+                    comment_num+=1
+                    comment_score /=sent_num
+                key_score+=comment_score
+            
+            if key_score!=0:
+                key_score/= comment_num
+            
+            if comment_num >=3:
+                d[i] = key_score
+        print(d)
+        return d
+        
+        
+if __name__ == '__main__':
+    link = "https://www.amazon.in/HP-Gaming-Mouse-G200-7QV30AA/dp/B08498186T"
+    link = "https://www.ebay.com/itm/185829460367"
+    #keys = get_keywords(link)
+    keys = ["weights"]  
+    get_sent(link,keys)
